d23765252e9c4a36deb4c39c606f1bcfcd4752b3
Naved Khan <navedkhan19032002@gmail.com>
2024-07-14 23:15:29 +0530

    FAQs fix

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -3,7 +3,7 @@ import { FaPhoneAlt, FaPlus } from "react-icons/fa";
 
 export default function FAQs() {
   return (
-    <div className="hidden grid-cols-12 gap-20 bg-[#F5E7E7] p-24 lg:grid">
+    <div className="grid-cols-1 gap-20 bg-[#F5E7E7] p-10 lg:grid lg:grid-cols-12 lg:p-24">
       <div className="col-span-5">
         <h1 className="text-3xl font-[400]">FAQs</h1>
         <h2 className="mb-10 mt-8 text-[#727272]">
@@ -15,11 +15,11 @@ export default function FAQs() {
           <p>Talk to our representative</p>
         </button>
       </div>
-      <div className="col-span-7">
+      <div className="col-span-7 mt-10 lg:mt-0">
         {faqs.map((faq, i) => (
           <div
             key={i}
-            className="flex items-center justify-between border border-t-black p-5 font-semibold"
+            className="flex items-center justify-between gap-5 border border-t-black p-5 font-semibold"
           >
             <p>{faq}</p>
             <FaPlus />
